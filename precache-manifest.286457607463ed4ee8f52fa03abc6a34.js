485f6acc7d4e50afdbe3dcee583c1cb0bfd4cdc8
mdanishu <mdanishu92@gmail.com>
2023-05-10 00:04:18 -0400

    Updates

---
+++ precache-manifest.286457607463ed4ee8f52fa03abc6a34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a90cb12ba4b10b9eca61",
+    "url": "/VidenStrategy/static/css/main.ae932a58.chunk.css"
+  },
+  {
+    "revision": "a90cb12ba4b10b9eca61",
+    "url": "/VidenStrategy/static/js/main.6007a922.chunk.js"
+  },
+  {
+    "revision": "d6fdedf0543361c07bb7",
+    "url": "/VidenStrategy/static/js/runtime~main.4ce970a5.js"
+  },
+  {
+    "revision": "6af03c7daefbc65ee85c",
+    "url": "/VidenStrategy/static/js/2.aa9c0696.chunk.js"
+  },
+  {
+    "revision": "bf3f3b7061bd69c08914",
+    "url": "/VidenStrategy/static/js/3.333ed05c.chunk.js"
+  },
+  {
+    "revision": "8cd1a6eb4ed85c2d1ebe43e705c96c07",
+    "url": "/VidenStrategy/index.html"
+  }
+];

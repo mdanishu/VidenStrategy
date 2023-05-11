1dcf5fea71778b6a039751452d4a848f4969eb13
mdanishu <mdanishu92@gmail.com>
2023-05-10 23:56:29 -0400

    delete other practice

--- Other Practice/HelloWorld.js
+++
@@ -1,6 +0,0 @@
-var msg = 'Hello World';
-console.log(msg);
-console.log('Danish is cool and likes to play games');
-
-let fname = 'test';
-fname =10;
--- Other Practice/HelloWorldType.ts
+++
@@ -1,1 +0,0 @@
-console.log('Hello World to Danish and Jean');

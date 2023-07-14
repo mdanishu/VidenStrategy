6e8a9cf757b8ef1240fa5f7106810a3dd21db831
mdanishu <mdanishu92@gmail.com>
2023-07-14 02:54:13 -0400

    fabars

--- VidenFrontEnd/src/components/NavbarElements.tsx
+++ VidenFrontEnd/src/components/NavbarElements.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { NavLink as Link } from "react-router-dom";
-import { FaBars } from "react-icons/fa";
+//import { FaBars } from "react-icons/fa";
 
 export const Nav = styled.nav`
   // Your Nav styles here
@@ -10,9 +10,9 @@ export const NavLink = styled(Link)`
   // Your NavLink styles here
 `;
 
-export const Bars = styled(FaBars)`
+//export const Bars = styled(FaBars)`
   // Your Bars styles here
-`;
+//`;
 
 export const NavMenu = styled.div`
   // Your NavMenu styles here

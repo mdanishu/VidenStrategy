a0880545b92fc0aee998257f0b70f42a9a12fe0d
mdanishu <mdanishu92@gmail.com>
2023-07-29 02:38:09 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ca3c6d74c783dc139911f9a29803dda.js"
+  "/precache-manifest.3a25ff1e67c1c144a79e71d0cede3cac.js"
 );
 
 workbox.clientsClaim();
